44d125d4be3220fa5848f4ac9c3128be9e563d93
formkergit <formker@gmail.com>
2026-01-14 16:03:38 +0100

    Ajout vue pour la rroute user

--- code/pages/controllers/user.php
+++ code/pages/controllers/user.php
@@ -4,18 +4,6 @@ $idUser = $_GET['id'] ?? null;
 if (! is_numeric($idUser)  ) {
     dd("Ce conducteur n'existe pas !!!");
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-</head>
-<body>
-    <h2>L'id user est : <?= $idUser ?></h2>
-    <p><a href="/" class="btn">Retour accueil</a></p>
 
-</body>
-</html>
+require 'pages/views/user-view.php';
+?>
---
+++ code/pages/views/user-view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <h2>L'id user est : <?= $idUser ?></h2>
+    <p><a href="/" class="btn">Retour accueil</a></p>
+
+</body>
+</html>
